4e4c9c56cf51e75214d99c2b449e4857fb0f05ab
Marijn Haverbeke <marijn@haverbeke.berlin>
2026-02-23 08:22:31 +0100

    Give contextual keywords a dynamic precedence
    Issue https://github.com/codemirror/lang-liquid/pull/15

--- test/test-liquid.ts
+++ test/test-liquid.ts
@@ -96,7 +96,7 @@ Template(CaseDirective(
 {% endfor %}
 `, `
 Template(ForDirective(
-  Tag(for,VariableName,in,VariableName,Parameter(ParameterName,":",NumberLiteral),Parameter(ParameterName,":",VariableName)),
+  Tag(for,VariableName,in,VariableName,Parameter(ParameterName,":",NumberLiteral),Parameter(ParameterName,":",continue)),
   Text, Interpolation(VariableName), Text,
   Tag(break), Text,
   Tag(continue), Text,
